d3121f89f5939ed995510c1e66532f759ecb02a0
DESKTOP-CAH5EGK\202-04 <dev@email.com>
2023-07-14 10:25:27 +0900

    새로시작

---
+++ workspace_java/JavaStudy/src/ch01_start/Algorithm.java
@@ -0,0 +1,16 @@
+package ch01_start;
+
+public class Algorithm {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println(f(2,3));
+	}
+	
+	public static int f(int a, int b) {
+		int sum;
+		sum = a + b;
+		return sum;
+	}
+}
---
+++ workspace_java/JavaStudy/src/ch02_variable/VariableName.java
@@ -0,0 +1,7 @@
+package ch02_variable;
+
+public class VariableName {
+	int todayIsHappy = 0; //데이터타입 + 변수명
+	//자바는 카멜표기법, 파이썬은 스네이크표기법
+	
+}
