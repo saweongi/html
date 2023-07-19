2e9ce5fe6faf2427034709146d124c7a9e9a0b34
DESKTOP-CAH5EGK\202-04 <dev@email.com>
2023-07-19 17:23:38 +0900

    학원수업

--- workspace_java/JavaStudy/src/practice/submult01/result03.java
+++ workspace_java/JavaStudy/src/practice/submult01/result03.java
@@ -5,27 +5,54 @@ import java.util.Scanner;
 public class result03 {
 
 	public static void main(String[] args) {
-		int example = 278;
-		int i= example /100;
-		int i1 = (example /10) %10;
-		int i2 = example%10;
-		int sum = i +i1 + i2;
-		System.out.println("결과는 :" +sum);
-		
-		String x = example + "";
-		
-		char y = x.charAt(0);
-		char y1 = x.charAt(1);
-		char y2 = x.charAt(2);
-	
-		int n =  (int)y - '0';
-		int n1 = (int)y1 - '0';
-		int n2 = (int)y2 - '0';
-		System.out.println(n);
-		System.out.println(n1);
-		System.out.println(n2);
-	
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("국어 점수를 입력해주세요");
+		int kor = scanner.nextInt();
+		System.out.println("영어 점수를 입력해주세요");
+		int eng = scanner.nextInt();
+		System.out.println("수학 점수를 입력해주세요");
+		int math = scanner.nextInt();
+
+		double sum = (kor + eng + math) / 3.0;
+		System.out.println("평균:" + sum);
+
+		if (sum >= 90) {
+			System.out.println("등급: A등급");
+		} else if (sum >= 80) {
+			System.out.println("등급: B등급");
+		} else if (sum >= 70) {
+			System.out.println("등급: C등급");
+		} else {
+			System.out.println("등급: D등급");
+		}
+
+		int sum1 = 1;
+		for (int i = 10; i >= 1; i--) {
+			sum1 *= i;
+			
+		}
+		System.out.println("결과:" + sum1);
+
+		long sum2 = 1;
+		for (int i = 15; i >= 1; i--) {
+			sum2 *= i;
+
+		}
+		System.out.println("결과:" + sum2);
+
+		
+		String findWally = "윌리울리일리울리울리일리월리일리윌리월리울리일리일리월리일리윌리일리윌리일리월리월리윌리울리윌리울리일리울리울리윌리일리";
+		sum1=findWally.length();
+		sum2=findWally.replace("월리","1").length();
+		System.out.println(sum1-sum2);
+		
+
+		String star = "******";
+		for (int i = 0; i <= 4; i++) {
+			System.out.println(star.substring(i,star.length()-1));
+		}
 	}
-
 }
+
+
