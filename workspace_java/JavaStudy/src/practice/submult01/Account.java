933098db11211b0fe532944c136da8e9ccad3f0b
saweongi <wwdnjsrl2@naver.com>
2023-07-30 19:59:52 +0900

    자바 과제완료

--- workspace_java/JavaStudy/src/practice/submult01/Account.java
+++ workspace_java/JavaStudy/src/practice/submult01/Account.java
@@ -1,19 +1,20 @@
 package practice.submult01;
 
+
+
 public class Account {
 
  String account_Member;
  String name;
- int first_Money;
-	Account(String account_Member, String name,int first_Money ){
+ String first_Money;
+
+ 
+	Account(String account_Member, String name,String first_Money ){
 		this.account_Member=account_Member;
-		this.name =  name;
+		this.name =name;
 		this.first_Money=first_Money;
-		
 	}
-	public void printAccount() {
-		System.out.println(account_Member+name +first_Money);
-	}
+	
 	
 	
 }
