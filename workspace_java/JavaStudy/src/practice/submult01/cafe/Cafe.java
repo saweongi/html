72d5628d37cc8af3137561e894c3251a8b7fbda5
saweongi <wwdnjsrl2@naver.com>
2023-08-01 21:34:03 +0900

    자바 카페과제끝

--- workspace_java/JavaStudy/src/practice/submult01/cafe/Cafe.java
+++ workspace_java/JavaStudy/src/practice/submult01/cafe/Cafe.java
@@ -4,39 +4,63 @@ import java.util.ArrayList;
 
 public class Cafe {
 	String name;
-	ArrayList menuList=new ArrayList();
+	ArrayList menuList = new ArrayList();
 
-	public Cafe(String name){
-		this.name =name;
+	public Cafe(String name) {
+		this.name = name;
 	}
-	
+
 	public void showMenu() {
 		if (this.name.equals("스타벅스")) {
-		Coffee coffee = new Coffee("아메리카노",5000);
-		Coffee coffee1 = new Coffee("카푸치노",6000);
-		Coffee coffee2 = new Coffee("오곡라떼",7000);
-		System.out.println("1. ["+coffee.name+": "+coffee.price+"]");
-		System.out.println("2. ["+coffee1.name+": "+coffee1.price+"]");
-		System.out.println("3. ["+coffee2.name+": "+coffee2.price+"]");
-	}
-		else if(this.name.equals("아카")) {
-			Coffee coffee = new Coffee("아메리카노",2500);
-			Coffee coffee1 = new Coffee("바닐라라떼",3000);
-			Coffee coffee2 = new Coffee("아이스티",3500);
-			System.out.println("1. ["+coffee.name+": "+coffee.price+"]");
-			System.out.println("2. ["+coffee1.name+": "+coffee1.price+"]");
-			System.out.println("3. ["+coffee2.name+": "+coffee2.price+"]");
+			Coffee coffee = new Coffee("아메리카노", 5000);
+			Coffee coffee1 = new Coffee("카푸치노", 6000);
+			Coffee coffee2 = new Coffee("오곡라떼", 7000);
+			System.out.println("1. [" + coffee.name + ": " + coffee.price + "]");
+			System.out.println("2. [" + coffee1.name + ": " + coffee1.price + "]");
+			System.out.println("3. [" + coffee2.name + ": " + coffee2.price + "]");
+		} else if (this.name.equals("아카")) {
+			Coffee coffee = new Coffee("아메리카노", 2500);
+			Coffee coffee1 = new Coffee("바닐라라떼", 3000);
+			Coffee coffee2 = new Coffee("아이스티", 3500);
+			System.out.println("1. [" + coffee.name + ": " + coffee.price + "]");
+			System.out.println("2. [" + coffee1.name + ": " + coffee1.price + "]");
+			System.out.println("3. [" + coffee2.name + ": " + coffee2.price + "]");
 		}
 	}
+
 	public void addCoffee() {
-		
+
 	}
-	
-	public Cafe(String name,ArrayList menList) {
-		this.name =name;
-		this.menuList =menuList;
+
+	public Cafe(String name, ArrayList menList) {
+		this.name = name;
+		this.menuList = menuList;
 	}
-	public void buyCoffee() {
+
+	public void buyCoffee(int cafe_menu) {
 		
+		if(this.name.equals("스타벅스")) {
+			if (cafe_menu ==1) {
+				System.out.println("아메리카노를 5000원에 구매했습니다.");
+			}else if (cafe_menu ==2) {
+				System.out.println("카푸치노를 6000원에 구매했습니다.");
+			}else if (cafe_menu ==3) {
+				System.out.println("오곡라떼를 7000원에 구매했습니다.");
+			
+				
+			
+	}
+		}else if (this.name.equals("아카")) {
+			if (cafe_menu ==1) {
+				System.out.println("아메리카노를 2500원에 구매했습니다.");
+			}else if (cafe_menu ==2) {
+				System.out.println("바닐라라떼를 3000원에 구매했습니다.");
+			}else if (cafe_menu ==3) {
+				System.out.println("아이스티를 3500원에 구매했습니다.");
+		}
 	}
 }
+}
+
+
+
--- workspace_java/JavaStudy/src/practice/submult01/cafe/CafeMain.java
+++ workspace_java/JavaStudy/src/practice/submult01/cafe/CafeMain.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CafeMain {
 
 	public static void main(String[] args) {
+		while(true) {
 		System.out.println("행동을 선택해주세요");
 		
 		System.out.println("1. 스타벅스 방문 | 2. 아카 방문 | 3. 사무실 복귀..");
@@ -19,18 +20,25 @@ public class CafeMain {
 			cafe.showMenu();
 			System.out.print("메뉴를 선택해주세요: ");
 			int cafe_menu =Integer.parseInt(scanner.nextLine());
+				cafe.buyCoffee(cafe_menu);
 			
 			
 			
-		
 			
 		}else if( cafe_sc ==2) {
 			Cafe cafe = new Cafe("아카");
 			System.out.println("어서오세요 "+cafe.name+" 입니다.");
 			cafe.showMenu();
+			System.out.print("메뉴를 선택해주세요: ");
 			int cafe_menu =Integer.parseInt(scanner.nextLine());
+			cafe.buyCoffee(cafe_menu);
 			
+		}else if (cafe_sc ==3) {
+			System.out.println("사무실로 복귀합니다..");
+			break;
 		}
 	}
 
 }
+}
+
