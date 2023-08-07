406e92b15c3eced34f2532e2cb562c75d4e75872
DESKTOP-CAH5EGK\202-04 <dev@email.com>
2023-08-07 12:08:44 +0900

    실습데이터 추가

--- workspace_java/JavaStudy/src/ch11_java_api/ApiJson.java
+++ workspace_java/JavaStudy/src/ch11_java_api/ApiJson.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DecimalFormat;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class ApiJson {
 
 	
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 
 		// https://api.upbit.com/v1/market/all
 
@@ -51,9 +56,55 @@ public class ApiJson {
 			in.close();
 			System.out.println(response.toString());
 		    JSONParser parser = new JSONParser();
-			
+		    JSONArray jsonArray = (JSONArray) parser.parse(response.toString());
+		    
+		    for(Object object: jsonArray) {
+		    	JSONObject jsonObj = (JSONObject) object;
+		    	System.out.println("Market: " + jsonObj.get("market"));
+		    	System.out.println("korean_name: " + jsonObj.get("korean_name"));
+		    	System.out.println("english_name: " + jsonObj.get("english_name"));
+		    }
+			System.out.println(getCoin("KRW-BTC"));
 		}
 
+	}
+	public static JSONObject getCoin(String market) {
+		
+		
+		JSONObject jsonObj = null;
+		URL url;
+		try {
+			 url = new URL("https://api.upbit.com/v1/ticker?markets=" + market);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while((inputLine = in.readLine())!= null) {
+				response.append(inputLine);
+			}
+			in.close();
+			JSONParser parser = new JSONParser();
+			JSONArray jsonArray = (JSONArray)parser.parse(response.toString());
+			jsonObj = (JSONObject)jsonArray.get(0);
+			System.out.println("trade date:" +jsonObj.get("trade_date"));
+			System.out.println("trade price:" +jsonObj.get("trade_price"));
+			//과학적 표기법으로 표현된 숫자 3.8515E7 <-- E에 오는 숫자는 10의 지수를 나타냄
+			// 3. 8515 * 10^7을 의미함 따라서 실제 값은 38515000 을 의미함
+			DecimalFormat decimalFormat = new DecimalFormat("#");
+			decimalFormat.setMaximumFractionDigits(0); //소수점 이하 0으로
+			System.out.println("trade_price:" + decimalFormat.format(jsonObj.get("trade_price")));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return jsonObj;
 	}
 
 }
